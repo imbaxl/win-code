e481b11efff0670c27bd20cbb469f27871b0c9e5
imbaxl <60095551+imbaxl@users.noreply.github.com>
2020-12-17 13:34:35 +0800

    Update plot.py

--- future/plot.py
+++ future/plot.py
@@ -12,23 +12,22 @@ for i in a:
 #学习matplotlib可视化
 
 #用pandas读取数据
-df1 = pd.read_csv('future/data.csv', index_col=0,)
+df1 = pd.read_excel('future/data/甲醇data.xlsx', index_col=0,)
 
 #修改字体以支持中文显示
 plt.rcParams['font.family']=['Heiti TC']
 
 #图示标题
-plt.title('PTA连续合约历史波动')
+plt.title('甲醇连续合约历史波动')
 
 #plt.plot(df1['最高价']-df1['最低价'], color='green',linewidth='2.0',linestyle='-')
 
 #定义随机变量
 mu = 0 #平均值
 sigma = 1 #标准差
-x = df1['最高价']-df1['最低价']
-y = st.norm.pdf(x, mu, sigma)
+x = df1['收盘价']
 
-plt.plot(x,y,color='blue',linewidth='1.0',linestyle='-')
+plt.plot(x,color='blue',linewidth='1.0',linestyle='-')
 
 plt.grid()
 plt.show()
