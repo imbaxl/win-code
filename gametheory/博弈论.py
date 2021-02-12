eb1533b32cc263b311b7a23756bcc517348986cb
imbaxl <60095551+imbaxl@users.noreply.github.com>
2021-02-12 14:12:37 +0800

    updated

--- gametheory/博弈论.py
+++ gametheory/博弈论.py
@@ -1,5 +1,24 @@
 class gametheory():
     def __init__(self):
         return
+        #完全信息静态博弈
+        #完全信息动态博弈
+        #不完全信息静态博弈
+        #不完全信息动态博弈
 
-    
+    def information_static_game(self):
+        #案例1：航空公司与旅行者博弈
+        """
+        参与者：2
+        支付：600-1000元
+        """
+        return
+
+    def information_dynamic_game(self):
+        return
+
+    def uninformation_static_game(self):
+        return
+
+    def uninformation_dynamic_game(self):
+        return
